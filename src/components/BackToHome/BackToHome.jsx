70df41beb0e84ef81f4175984f8a2c1fdde27fe5
Kauasalya <kausalya.546@gmail.com>
2026-08-21 22:22:29 +0530

    added

--- src/components/BackToHome/BackToHome.jsx
+++ src/components/BackToHome/BackToHome.jsx
@@ -1,7 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./BackToHome.css";
 
 function BackToHome() {
+  const location = useLocation();
+
+  // Don't show on Home page
+  if (location.pathname === "/") {
+    return null;
+  }
+
   return (
     <Link to="/" className="back-to-home">
       ← HOME
